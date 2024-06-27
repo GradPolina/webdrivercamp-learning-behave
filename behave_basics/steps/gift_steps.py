a80739a522b86c5bb7a3c3422c609e51ad29e144
GradPolina <Gradinar_polina@yahoo.com>
2024-06-27 14:56:54 -0500

    Done task #0

--- behave_basics/steps/gift_steps.py
+++ behave_basics/steps/gift_steps.py
@@ -1,1 +1,7 @@
+from behave import *
+from selenium import webdriver
+@step('Navigate to {url}')
+def step_impl(context, url):
+    context.browser = webdriver.Chrome()
+    context.browser.get(url)
 
