04827c25c58b2861dd4e6395df4899c7b4b5ae36
GradPolina <Gradinar_polina@yahoo.com>
2024-07-05 21:12:20 -0500

    Updeted to tasks #3

--- behave_basics/components/base.py
+++ behave_basics/components/base.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 class Base:
     def __init__(self, driver, timeout=10):
         self.driver = driver
@@ -8,11 +9,14 @@ class Base:
     def navigate_to(self, url):
         self.driver.get(url)
     def click(self, locator):
-        element = (WebDriverWait(self.driver, self.timeout).until
-                   (EC.element_to_be_clickable(locator))
-                   )
+        element = WebDriverWait(self.driver, self.timeout).until(
+            EC.element_to_be_clickable(locator)
+        )
         element.click()
-    def find_element(self, locator):
-        return (WebDriverWait(self.driver, self.timeout).until
-                (EC.presence_of_element_located(locator))
-                )
+    def find_visible_element(self, locator):
+        return (WebDriverWait(self.driver, self.timeout).until(
+            EC.visibility_of_element_located((By.XPATH, locator))))
+    def send_keys(self, locator, text):
+        element = self.find_element(locator)
+        element.clear()
+        element.send_keys(text)
