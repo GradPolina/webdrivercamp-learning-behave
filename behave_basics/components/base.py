66a744e01a3a31799f5750e3a211e597f9bba7bd
GradPolina <Gradinar_polina@yahoo.com>
2024-07-07 14:45:44 -0500

    Executed

--- behave_basics/components/base.py
+++ behave_basics/components/base.py
@@ -6,16 +6,28 @@ class Base:
     def __init__(self, driver, timeout=10):
         self.driver = driver
         self.timeout = timeout
+
     def navigate_to(self, url):
         self.driver.get(url)
+
     def click(self, locator):
         element = WebDriverWait(self.driver, self.timeout).until(
             EC.element_to_be_clickable(locator)
         )
         element.click()
+
     def find_visible_element(self, locator):
         return (WebDriverWait(self.driver, self.timeout).until(
             EC.visibility_of_element_located((By.XPATH, locator))))
+
+    def find_all_elements(self, locator):
+        return (WebDriverWait(self.driver, self.timeout).until(
+            EC.presence_of_all_elements_located((By.XPATH, locator))))
+
+    def find_elements(self, locator):
+        return (WebDriverWait(self.driver, self.timeout).until(
+            EC.presence_of_element_located((By.XPATH, locator))))
+
     def send_keys(self, locator, text):
         element = self.find_element(locator)
         element.clear()
