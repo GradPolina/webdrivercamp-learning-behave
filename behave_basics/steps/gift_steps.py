45508c2ce2f2a96e89cb4063ac405c199584519c
GradPolina <Gradinar_polina@yahoo.com>
2024-07-07 23:40:02 -0500

    Executed all scenarios

--- behave_basics/steps/gift_steps.py
+++ behave_basics/steps/gift_steps.py
@@ -38,39 +38,63 @@ def step_impl(context, search_item):
     header_text = result_header.text
     assert search_item.lower() in header_text.lower(), f"Header doesn't contain '{search_item}': {header_text}"
 
-@step("Collect all items on the first page into {context_var}")
-def step_impl(context, context_var):
+@step("Collect all items on the first page into {var}")
+@step("Collect all items on the first page into {var} on the {level} level")
+def step_impl(context, var, level=None):
     prices_xpath = (f'//div[@data-test="@web/ProductCard/ProductCardVariantDefault"]'
                     f'//span[@data-test="current-price"]')
     items_xpath = (f'//div[@data-test="@web/ProductCard/ProductCardVariantDefault"]'
                    f'//a[@data-test="product-title"]')
+    shipping_xpath = (f'//div[@data-test="@web/ProductCard/ProductCardVariantDefault"]'
+                      f'//span[contains(text(), "Ships free")]')
 
     results_price = Base(context.browser).find_all_elements(prices_xpath)
     results_items = Base(context.browser).find_all_elements(items_xpath)
+    results_shipping = Base(context.browser).find_all_elements(shipping_xpath)
 
     collected_items = []
 
     all_prices = [float(price.text.replace("$", "").split()[-1]) for price in results_price]
     all_items = [item.text for item in results_items]
+    all_shipping = [shipping.text for shipping in results_shipping]
 
-    for price, item in zip(all_items, all_prices):
-        collected_items.append({'title': item, 'price': price})
-    setattr(context, context_var, collected_items)
+    for price, item, shipping in zip(all_items, all_prices, all_shipping):
+        collected_items.append({'title': item, 'price': price, 'shipping': shipping})
+    if level == 'feature':
+        setattr(context.feature, var, collected_items)
+    else:
+        setattr(context, var, collected_items)
 
-@step("Verify all collected results' prices is {condition}")
-def step_impl(context, condition):
-    collected_items = context.collected_items
-    mismatch = {}
-    for item in collected_items:
-        price = item['price']
-        if condition == "<":
-            condition, exp_price = condition.strit().split()
-            if float(price) >= float(exp_price):
-                mismatch[item['title']] = price
+@step("Verify all collected results' {param} is {condition}")
+def step_impl(context, param, condition):
+    collected_items = context.feature.collected_items
+    if param == 'price':
+        mismatch = {}
+        for item in collected_items:
+            value = item['price']
+            if condition == "<":
+                condition, exp_price = condition.strit().split()
+                if float(value) >= float(exp_price):
+                    mismatch[item['title']] = value
 
-    if mismatch:
-        for item, price in mismatch.items():
-            price(f'Item {item} has a price of {price}')
-            raise AssertionError("Some items have prices greater than the expected value.")
+        if mismatch:
+            for title, value in mismatch.items():
+                print(f'Item {title} has a price of {value}')
+                raise AssertionError("Some items have prices greater than the expected value.")
+
+    elif param == 'shipping':
+        mismatch = {}
+        for item in collected_items:
+            if item['shipping'] != condition:
+                mismatch[item['title']] = item['shipping']
+        if mismatch:
+            for title, shipping in mismatch.items():
+                print(f'Item {title} has a price of {shipping}')
+                raise AssertionError("Some items do not have the expected shipment condition.")
+
+@step('Print the current url')
+def step_impl(context):
+    current_url = context.browser.current_url
+    print(f"Current URL: {current_url}")
 
 
